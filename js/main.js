4b6a7829df620fc9ac2d02ff6f017008cd9d080e
codebyfauzan <fauzanreza23@gmail.com>
2021-09-10 01:20:04 +0700

    fixing bugs

--- js/main.js
+++ js/main.js
@@ -2,31 +2,54 @@ document.addEventListener('DOMContentLoaded', function() {
     // Calling function displayCurrentTime every one second
     setInterval(displayCurrentTime, 1000);
 
-
     // Get button elements
     const startButton = document.getElementById('start');
     const resetButton = document.getElementById('reset');
+    const iconButton = document.querySelector('#start i');
     const doneButtons = document.getElementsByClassName('finish');
     const participantsTime = document.getElementsByClassName('participant-time');
 
-    startButton.onclick = () => start();
+    startButton.onclick = () => {
+        // When iconButton is play button
+        if(iconButton.classList.contains('bi-play-fill')) {
+            start();
+            iconButton.classList.remove('bi-play-fill');
+            iconButton.classList.add('bi-pause-fill');
+        } 
+         // When iconButton is pause button
+        else if(iconButton.classList.contains('bi-pause-fill')) {
+            pause();
+            iconButton.classList.remove('bi-pause-fill');
+            iconButton.classList.add('bi-play-fill');
+        }
+    };
+
     resetButton.onclick = () => {
         reset();
+        // Clearing All effects and class back to default
         for(element of participantsTime) {
             element.classList.remove('done');
+            element.classList.remove('blink');
+        }
+        if(iconButton.classList.contains('bi-pause-fill')) {
+            iconButton.classList.remove('bi-pause-fill');
+            iconButton.classList.add('bi-play-fill');
         }
     };
 
     Array.from(doneButtons).forEach((element, index) => {
-
+        // When doneButton get clicked
         element.addEventListener('click', function() {
-            const timeText = participantsTime[index].innerText;
+            // Do nothing when timer doesn't start and pTime is done
+            if(second == 0 || participantsTime[index].classList.contains('done')) {
+                return;
+            }
+            // Adding effects to pTime when it's done
             participantsTime[index].classList.add('done');
             participantsTime[index].classList.add('blink');
             setTimeout(function(){ 
                 participantsTime[index].classList.remove('blink');
             }, 3000);
-            console.log(timeText);
         });
 
     });
--- js/script.js
+++ js/script.js
@@ -1,22 +1,16 @@
-// Adding zero in front of number
-function addZero(input) {
-  return input < 10 ? `0${input}` : input;
-}
-  
 function displayCurrentTime() {
-  var date = new Date();
+  let date = new Date();
   const currentTime = document.querySelector('.current-time');
-  var h = addZero(date.getHours());
-  var m = addZero(date.getMinutes());
-  var s = addZero(date.getSeconds());
+  let h = date.getHours().toString().padStart(2, "0");
+  let m = date.getMinutes().toString().padStart(2, "0");
+  let s = date.getSeconds().toString().padStart(2, "0");
   currentTime.innerHTML = `${h}:${m}:${s}`;
 }
 
 // Declaring timer variables
-let hour = 0;
-let minute = 0;
-let second = 0;
-let cron;
+let startTime;
+let elapsedTime = 0;
+let timerInterval;
 
 // Declaring component variables
 const participantsTime = document.getElementsByClassName('participant-time');
@@ -24,42 +18,55 @@ const stopwatchTime = document.querySelector('.timer h1');
 
 // Start timer
 function start() {
-  clearInterval(cron);
-  cron = setInterval(() => { timer(); }, 1000);
+  startTime = Date.now() - elapsedTime;
+  timerInterval = setInterval(() => {
+    elapsedTime = Date.now() - startTime;
+    timeToDisplay(elapsedTime);
+  }, 10);
+}
+
+// Pause timer
+function pause() {
+  clearInterval(timerInterval);
 }
 
 // Reset timer
 function reset() {
-  clearInterval(cron);
-  hour = 0;
-  minute = 0;
-  second = 0;
+  clearInterval(timerInterval);
+  elapsedTime = 0;
   
-  stopwatchTime.innerText = '00:00:00';
+  stopwatchTime.innerText = '00:00.00';
   
   for (let item of participantsTime) {
-    item.innerText = '00:00:00';
+    item.innerText = '00:00.00';
   }
   
 }
 
-function timer() {
-  second++;
-  if (second == 60) {
-    second = 0;
-    minute++;
-  }
-  if (minute == 60) {
-    minute = 0;
-    hour++;
-  }
+function timeToDisplay(time) {
 
-  stopwatchTime.innerText = `${addZero(hour)}:${addZero(minute)}:${addZero(second)}`;
+  let diffInHrs = time / 3600000;
+  hour = Math.floor(diffInHrs);
+
+  let diffInMin = (diffInHrs - hour) * 60;
+  minute = Math.floor(diffInMin);
+
+  let diffInSec = (diffInMin - minute) * 60;
+  second = Math.floor(diffInSec);
+
+  let diffInMs = (diffInSec - second) * 100;
+  milisecond = Math.floor(diffInMs);
+
+  let formattedMM = minute.toString().padStart(2, "0");
+  let formattedSS = second.toString().padStart(2, "0");
+  let formattedMS = milisecond.toString().padStart(2, "0");
+
+  stopwatchTime.innerText = `${formattedMM}:${formattedSS}.${formattedMS}`;
   
   for (let item of participantsTime) {
     if(item.classList.length == 1) {
-      item.innerText = `${addZero(hour)}:${addZero(minute)}:${addZero(second)}`;
+      item.innerText = `${formattedMM}:${formattedSS}.${formattedMS}`;
     }
   }
- 
+
 }
